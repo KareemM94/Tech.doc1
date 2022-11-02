b5fa9e0704b5968f635da09c9fec65b7e7a32a0a
Account <accountholder@Accounts-MacBook-Pro.local>
2022-11-02 16:30:35 +0000

    write anything

--- script.js
+++ script.js
@@ -22,18 +22,20 @@ window.addEventListener('scroll', ()=> {
 })
 
 
-document.addEventListener("click", e => {
-    const isDropdownButton = e.target.matches("[data-dropdown-button]")
-    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+// STUPID HOVER STYLING
 
-    let currentDropdown
-    if(isDropdownButton) {
-        currentDropdown = e.target.closest('[data-dropdown]')
-        currentDropdown.classList.toggle('active')
-    }
+// document.addEventListener("click", e => {
+//     const isDropdownButton = e.target.matches("[data-dropdown-button]")
+//     if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
 
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-        if (dropdown === currentDropdown) return
-        dropdown.classList.remove('active')
-    })
-})
+//     let currentDropdown
+//     if(isDropdownButton) {
+//         currentDropdown = e.target.closest('[data-dropdown]')
+//         currentDropdown.classList.toggle('active')
+//     }
+
+//     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+//         if (dropdown === currentDropdown) return
+//         dropdown.classList.remove('active')
+//     })
+// })
